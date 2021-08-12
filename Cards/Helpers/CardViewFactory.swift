bf5f7511e94905ea58851be59ecc4cea95d0868d
Shliakhin Sergei <sshlyahin@yahoo.com>
2021-08-12 22:47:28 +0300

    feat: add Factory for CardView

---
+++ Cards/Helpers/CardViewFactory.swift
@@ -0,0 +1,50 @@
+//
+//  CardViewFactory.swift
+//  Cards
+//
+//  Created by SERGEY SHLYAKHIN on 12.08.2021.
+//
+
+import UIKit
+
+class CardViewFactory {
+    func get(_ shape: CardType, withSize size: CGSize, andColor color: CardColor) -> UIView {
+        // на основе размера определяем фрейм
+        let frame = CGRect(origin: .zero, size: size)
+        // определяем UI - цвет на основе цвета модели
+        let viewColor = getViewColorBy(modelColor: color)
+        
+        // генерируем и возвращаем карточку
+        switch shape {
+        case .circle:
+            return CardView<CircleShape>(frame: frame, color: viewColor)
+        case .cross:
+            return CardView<CrossShape>(frame: frame, color: viewColor)
+        case .square:
+            return CardView<SquareShape>(frame: frame, color: viewColor)
+        case .fill:
+            return CardView<FillShape>(frame: frame, color: viewColor)
+        }
+    }
+    
+    private func getViewColorBy(modelColor: CardColor) -> UIColor {
+        switch modelColor {
+        case .black:
+            return .black
+        case .red:
+            return .red
+        case .green:
+            return .green
+        case .gray:
+            return .gray
+        case .brown:
+            return .brown
+        case .yellow:
+            return .yellow
+        case .purple:
+            return .purple
+        case .orange:
+            return .orange
+        }
+    }
+}
