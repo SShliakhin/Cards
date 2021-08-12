79957e2f842b6668bac98627262ac0aab7b7e4de
Shliakhin Sergei <sshlyahin@yahoo.com>
2021-08-12 22:25:26 +0300

    feat: add button and gameBoard on the View

--- Cards/Controller/BoardGameController.swift
+++ Cards/Controller/BoardGameController.swift
@@ -13,6 +13,8 @@ class BoardGameController: UIViewController {
     var cardsPairsCounts = 8
     // сущность "Игра"
     lazy var game: Game = getNewGame()
+    lazy var startButtonView = getStartButtonView()
+    lazy var boardGameView = getBoardGameView()
     
     private func getNewGame() -> Game {
         let game = Game()
@@ -21,21 +23,65 @@ class BoardGameController: UIViewController {
         return game
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private func getStartButtonView() -> UIButton {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        
+        button.center.x = view.center.x
+        
+        // привязка к Safe Area, через окно приложения
+        let window = UIApplication.shared.windows[0]
+        let topPadding = window.safeAreaInsets.top
+        button.frame.origin.y = topPadding
+        
+        button.setTitle("Начать игру", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.gray, for: .highlighted)
+        button.backgroundColor = .systemGray4
+        button.layer.cornerRadius = 10
+        
+        // подключаем обработчик
+        button.addTarget(nil, action: #selector(startGame(_:)), for: .touchUpInside)
+        
+        return button
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func getBoardGameView() -> UIView {
+        // отступ от ближайших элементов
+        let margin: CGFloat = 10
+        let boardView = UIView()
+        
+        boardView.frame.origin.x = margin
+        let window = UIApplication.shared.windows[0]
+        let topPadding = window.safeAreaInsets.top
+        boardView.frame.origin.y = topPadding + startButtonView.frame.height + margin
+        
+        // расчет ширины
+        boardView.frame.size.width = UIScreen.main.bounds.width - margin * 2
+        // расчет высоты с учетом нижнего отступа
+        let bottomPadding = window.safeAreaInsets.bottom
+        boardView.frame.size.height = UIScreen.main.bounds.height - boardView.frame.origin.y - margin - bottomPadding
+        
+        // стиль игрового поля
+        boardView.layer.cornerRadius = 5
+        boardView.backgroundColor = UIColor(red: 0.1, green: 0.9, blue: 0.1, alpha: 0.3)
+        
+        return boardView
     }
-    */
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.addSubview(startButtonView)
+        view.addSubview(boardGameView)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @objc
+    func startGame(_ sender: UIButton) {
+        
+    }
 
 }
