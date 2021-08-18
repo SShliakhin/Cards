212778b5a6cbea31d99e49d89de977791aa0be2f
Shliakhin Sergei <sshlyahin@yahoo.com>
2021-08-18 18:31:36 +0300

    feat: add cards on the start Controller

--- Cards/Controller/BoardGameController.swift
+++ Cards/Controller/BoardGameController.swift
@@ -9,19 +9,19 @@ import UIKit
 
 class BoardGameController: UIViewController {
 
+    // MARK: - Properties
+    
     // количество пар уникальных карточек
     var cardsPairsCounts = 8
-    // количество переворотов
-    lazy var flipsCount = 0 {
-        didSet {
-            game.flipsCount = flipsCount
-            title = "Flips: \(flipsCount)"
-        }
-    }
     // сущность "Игра"
     lazy var game: Game = getNewGame()
-    //lazy var startButtonView = getStartButtonView()
-    //lazy var flipButtonView = getFlipButtonView()
+    
+    // игральные карточки
+    var cardViews = [UIView]()
+    // перевернутые карточки
+    private var flippedCards = [UIView]()
+    
+    // поле для игры
     lazy var boardGameView = getBoardGameView()
     
     // размеры карточек
@@ -36,68 +36,41 @@ class BoardGameController: UIViewController {
         Int(boardGameView.frame.height - cardSize.height)
     }
     
-    // игральные карточки
-    var cardViews = [UIView]()
-    private var flippedCards = [UIView]()
-    
-    private func getNewGame() -> Game {
-        let game = Game()
-        game.cardsCount = self.cardsPairsCounts
-        game.generateCards()
-        return game
+    // количество переворотов
+    lazy var flipsCount = 0 {
+        didSet {
+            // запоминаем в моделе
+            game.flipsCount = flipsCount
+            // обновляем на экране
+            title = "Flips: \(flipsCount)"
+        }
     }
     
-    private func getStartButtonView() -> UIButton {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 180, height: 50))
-        
-        button.center.x = view.center.x
-        
-        // привязка к Safe Area, через окно приложения
-        let window = UIApplication.shared.windows[0]
-        let topPadding = window.safeAreaInsets.top
-        button.frame.origin.y = topPadding
-        
-        button.setTitle("Начать игру", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.setTitleColor(.gray, for: .highlighted)
-        button.backgroundColor = .systemGray4
-        button.layer.cornerRadius = 10
-        
-        // подключаем обработчик
-        button.addTarget(nil, action: #selector(startGame(_:)), for: .touchUpInside)
-        
-        return button
+    // MARK: - Life cycle
+    
+    override func loadView() {
+        super.loadView()
+        view.addSubview(boardGameView)
     }
     
-    private func getFlipButtonView() -> UIButton {
-        let margin: CGFloat = 10
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-        
-        button.frame.origin.x = margin
-        
-        // привязка к Safe Area, через окно приложения
-        let window = UIApplication.shared.windows[0]
-        let topPadding = window.safeAreaInsets.top
-        button.frame.origin.y = topPadding
-        
-        button.setTitle("<>", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.setTitleColor(.gray, for: .highlighted)
-        button.backgroundColor = .systemGray4
-        button.layer.cornerRadius = 10
-        
-        // подключаем обработчик
-        button.addAction(UIAction(handler: { _ in
-            let flippedCount = (self.cardViews as! [FlippableView]).filter({ $0.isFlipped }).count
-            let isFlipped = flippedCount < self.cardViews.count
-            for card in self.cardViews {
-                (card as! FlippableView).isFlipped = isFlipped
-            }
-            self.flippedCards = []
-        }), for: .touchUpInside)
-        
-        return button
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Flips: XXX"
+        view.backgroundColor = .lightGray
+        navigationController?.navigationBar.prefersLargeTitles = false
+        let flipButton = UIBarButtonItem(image: UIImage(systemName: "doc.on.doc")!,
+                                          style: .plain,
+                                          target: self,
+                                          action: #selector(actionAllFlip))
+        navigationItem.rightBarButtonItem = flipButton
+        start()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK: - Custom methods
     
     private func getBoardGameView() -> UIView {
         // отступ от ближайших элементов
@@ -123,6 +96,66 @@ class BoardGameController: UIViewController {
         boardView.backgroundColor = .brown
         
         return boardView
+    }
+    
+    private func getNewGame() -> Game {
+        let game = Game()
+        game.cardsCount = self.cardsPairsCounts
+        game.generateCards()
+        return game
+    }
+    
+    private func start() {
+        let alertController = UIAlertController(title: "Continue previous game?", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Cancel",
+                                                style: .cancel) { _ in
+            self.game = self.getNewGame()
+            let cards = self.getCardBy(modelData: self.game.cards)
+            self.placeCardsOnBoard(cards)
+        })
+        alertController.addAction(UIAlertAction(title: "OK",
+                                                style: .default) { _ in })
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    private func getCardBy(modelData: [Card]) -> [UIView] {
+        var cardViews = [UIView]()
+        let cardViewFactory = CardViewFactory()
+        for (index, modelCard) in modelData.enumerated() {
+            let cardOne = cardViewFactory.get(modelCard.type, withSize: cardSize, andColor: modelCard.color)
+            cardOne.tag = index
+            cardViews.append(cardOne)
+            let cardTwo = cardViewFactory.get(modelCard.type, withSize: cardSize, andColor: modelCard.color)
+            cardTwo.tag = index
+            cardViews.append(cardTwo)
+        }
+        // добавляем всем картам обработчик переворота
+        for card in cardViews {
+            (card as! FlippableView).flipCompletionHandler = { [self] flippedCard in
+                // переносим карточку вверх иерархии
+                flippedCard.superview?.bringSubviewToFront(flippedCard)
+                
+                // добавляем или удаляем карточку в/из перевернутых
+                addOrRemoveCard(flippedCard)
+                // проверяем карты
+                checkCards()
+            }
+        }
+        return cardViews
+    }
+    
+    private func placeCardsOnBoard(_ cards: [UIView]) {
+        // удаляем все имеющиеся на игровом поле карточки
+        for card in cardViews {
+            card.removeFromSuperview()
+        }
+        cardViews = cards
+        for card in cardViews {
+            let randomX = Int.random(in: 0...cardMaxXCoordinate)
+            let randomY = Int.random(in: 0...cardMaxYCoordinate)
+            card.frame.origin = CGPoint(x: randomX, y: randomY)
+            boardGameView.addSubview(card)
+        }
     }
     
     private func addOrRemoveCard(_ card: FlippableView) {
@@ -183,81 +216,10 @@ class BoardGameController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    private func getCardBy(modelData: [Card]) -> [UIView] {
-        var cardViews = [UIView]()
-        let cardViewFactory = CardViewFactory()
-        for (index, modelCard) in modelData.enumerated() {
-            let cardOne = cardViewFactory.get(modelCard.type, withSize: cardSize, andColor: modelCard.color)
-            cardOne.tag = index
-            cardViews.append(cardOne)
-            let cardTwo = cardViewFactory.get(modelCard.type, withSize: cardSize, andColor: modelCard.color)
-            cardTwo.tag = index
-            cardViews.append(cardTwo)
-        }
-        // добавляем всем картам обработчик переворота
-        for card in cardViews {
-            (card as! FlippableView).flipCompletionHandler = { [self] flippedCard in
-                // переносим карточку вверх иерархии
-                flippedCard.superview?.bringSubviewToFront(flippedCard)
-                
-                // добавляем или удаляем карточку в/из перевернутых
-                addOrRemoveCard(flippedCard)
-                // проверяем карты
-                checkCards()
-            }
-        }
-        return cardViews
-    }
-    
-    private func placeCardsOnBoard(_ cards: [UIView]) {
-        // удаляем все имеющиеся на игровом поле карточки
-        for card in cardViews {
-            card.removeFromSuperview()
-        }
-        cardViews = cards
-        for card in cardViews {
-            let randomX = Int.random(in: 0...cardMaxXCoordinate)
-            let randomY = Int.random(in: 0...cardMaxYCoordinate)
-            card.frame.origin = CGPoint(x: randomX, y: randomY)
-            boardGameView.addSubview(card)
-        }
-    }
-    
-    override func loadView() {
-        super.loadView()
-        
-        //view.addSubview(startButtonView)
-        //view.addSubview(flipButtonView)
-        view.addSubview(boardGameView)
-        start()
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Flips: XXX"
-        view.backgroundColor = .lightGray
-        navigationController?.navigationBar.prefersLargeTitles = false
-        let flipButton = UIBarButtonItem(image: UIImage(systemName: "doc.on.doc")!,
-                                          style: .plain,
-                                          target: self,
-                                          action: #selector(action))
-        navigationItem.rightBarButtonItem = flipButton
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-//        navigationController?.navigationBar.prefersLargeTitles = false
-    }
+    // MARK: - Actions
     
     @objc
-    func startGame(_ sender: UIButton) {
-        game = getNewGame()
-        let cards = getCardBy(modelData: game.cards)
-        placeCardsOnBoard(cards)
-    }
-    
-    @objc
-    func action() {
+    private func actionAllFlip() {
         let flippedCount = (self.cardViews as! [FlippableView]).filter({ $0.isFlipped }).count
         let isFlipped = flippedCount < self.cardViews.count
         for card in self.cardViews {
@@ -265,18 +227,4 @@ class BoardGameController: UIViewController {
         }
         self.flippedCards = []
     }
-    
-    func start() {
-        let alertController = UIAlertController(title: "Continue previous game?", message: nil, preferredStyle: .actionSheet)
-        alertController.addAction(UIAlertAction(title: "Cancel",
-                                                style: .cancel) { _ in
-            self.game = self.getNewGame()
-            let cards = self.getCardBy(modelData: self.game.cards)
-            self.placeCardsOnBoard(cards)
-        })
-        alertController.addAction(UIAlertAction(title: "OK",
-                                                style: .default) { _ in })
-        self.present(alertController, animated: true, completion: nil)
-    }
-
 }
--- Cards/Controller/StartController.swift
+++ Cards/Controller/StartController.swift
@@ -16,6 +16,10 @@ class StartController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Начать игру", for: .normal)
         button.backgroundColor = .green
+        button.layer.cornerRadius = 10
+        button.addAction(UIAction(handler: {_ in
+            self.navigationController?.pushViewController(BoardGameController(), animated: true)
+        }), for: .touchUpInside)
         return button
     }()
     lazy var settingsButtonView : UIButton = {
@@ -23,8 +27,22 @@ class StartController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Настройки", for: .normal)
         button.backgroundColor = .green
+        button.layer.cornerRadius = 10
+        button.addAction(UIAction(handler: {_ in
+            self.navigationController?.pushViewController(SettingsController(), animated: true)
+        }), for: .touchUpInside)
         return button
     }()
+    
+    lazy var miniBoardGameView : UIView = {
+        let board = UIView()
+        board.translatesAutoresizingMaskIntoConstraints = false
+        board.backgroundColor = .lightGray
+        board.layer.cornerRadius = 10
+        return board
+    }()
+    
+    var cardViews = [UIView]()
     
     // MARK: - Life cycle
     
@@ -36,12 +54,6 @@ class StartController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Cards"
-        startButtonView.addAction(UIAction(handler: {_ in
-            self.navigationController?.pushViewController(BoardGameController(), animated: true)
-        }), for: .touchUpInside)
-        settingsButtonView.addAction(UIAction(handler: {_ in
-            self.navigationController?.pushViewController(SettingsController(), animated: true)
-        }), for: .touchUpInside)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,11 +61,17 @@ class StartController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupCards()
+    }
+    
     // MARK: -  Custom methods
     
     private func setupViews() {
         view.addSubview(startButtonView)
         view.addSubview(settingsButtonView)
+        view.addSubview(miniBoardGameView)
         setupConstraints()
     }
     
@@ -61,14 +79,62 @@ class StartController: UIViewController {
         let constraints = [
             startButtonView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             startButtonView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 3 / 4),
-            startButtonView.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
-            startButtonView.heightAnchor.constraint(equalToConstant: 50),
+            startButtonView.heightAnchor.constraint(equalToConstant: 30),
+            
             settingsButtonView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             settingsButtonView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 3 / 4),
-            settingsButtonView.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 50 ),
-            settingsButtonView.heightAnchor.constraint(equalToConstant: 50),
+            settingsButtonView.heightAnchor.constraint(equalToConstant: 30),
+            
+            miniBoardGameView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            miniBoardGameView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            miniBoardGameView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            
+            settingsButtonView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20 ),
+            startButtonView.bottomAnchor.constraint(equalTo: settingsButtonView.topAnchor, constant: -10),
+            miniBoardGameView.bottomAnchor.constraint(equalTo: startButtonView.topAnchor, constant: -10)
         ]
         NSLayoutConstraint.activate(constraints)
+    }
+    
+    private func setupCards() {
+        // определяем размер карточки
+        let minSide = min(miniBoardGameView.bounds.width, miniBoardGameView.bounds.height)
+        let widthCard = minSide / 2
+        let heightCard = widthCard / 3 * 4
+        
+        guard widthCard != cardViews.first?.bounds.width else {
+            return
+        }
+        
+        // размеры изменились
+        // удаляем старые карточки
+        cardViews.forEach{ card in
+            card.removeFromSuperview()
+        }
+        cardViews = []
+        
+        // создаем новые карточки
+        let cardViewFactory = CardViewFactory()
+        let cardOne = cardViewFactory.get(CardType.circle , withSize: CGSize(width: widthCard, height: heightCard), andColor: CardColor.green)
+        let cardTwo = cardViewFactory.get(CardType.square , withSize: CGSize(width: widthCard, height: heightCard), andColor: CardColor.red)
+        // добавляем на поле
+        miniBoardGameView.addSubview(cardOne)
+        miniBoardGameView.addSubview(cardTwo)
+        // и в массив
+        cardViews.append(cardOne)
+        cardViews.append(cardTwo)
+        // определяем координаты origin
+        let marginX = (miniBoardGameView.bounds.width - widthCard * 1.5) / 2
+        cardOne.frame.origin.x = marginX
+        cardTwo.frame.origin.x = marginX + widthCard * 0.5
+        
+        if heightCard * 1.5 <= miniBoardGameView.bounds.height {
+            cardOne.frame.origin.y = (miniBoardGameView.bounds.height - heightCard * 1.5) / 2
+            cardTwo.frame.origin.y = cardOne.frame.origin.y + heightCard * 0.5
+        } else {
+            cardOne.frame.origin.y = 0
+            cardTwo.frame.origin.y = miniBoardGameView.bounds.height - heightCard
+        }
     }
 
 }
